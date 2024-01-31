447d4f155dfb6169d66fb0a7cb136b92b1196a7c
Lakshk25 <raghavkumrawat01039@gmail.com>
2024-01-31 16:28:58 +0530

    code fixes

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
 const express = require('express');
 const userRoute = require('./routes/user');
 const cors = require('cors');
-require('./db/connect')
+const path = require('path'); // Import the 'path' module
+require('./db/connect');
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 app.use(cors({
-    origin: 'http://localhost:5500', // Replace with your React app's URL
     origin: 'https://lakshsite.netlify.app', // Replace with your React app's URL
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
-  }));
+}));
+
 app.use('/api', userRoute);
+app.get('/', (req, res) => {
+  res.send("running...");
+})
+
+// Serve static assets (like CSS and JS files) from the 'build' folder
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
--- routes/user.js
+++ routes/user.js
@@ -2,16 +2,15 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
 
-router.get('/', async(req, res) =>{
+router.get('/', async (req, res) => {
   res.send("runn")
 })
 router.post('/send', async (req, res) => {
-    console.log(req.body)
+  console.log(req.body)
   try {
     const { name, email, message } = req.body;
     const newUser = new User({ name, email, message });
     await newUser.save();
-    res.status(201).json(newUser);
   } catch (error) {
     console.error('Error creating user:', error);
     res.status(500).json({ error: 'An error occurred' });
